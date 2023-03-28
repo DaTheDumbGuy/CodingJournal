7afa07c2762014193f54573a489c16b3afce0cbc
Daryl Bacurin <darylbacurin14@gmail.com>
2023-03-28 18:51:31 +0800

    [Update] New added Update feater

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import MyForm from './components/MyForm';
 import MyForm2 from './components/MyForm2';
+
 function App() {
   return (
     <div className="App">
       <MyForm />
       <MyForm2 />
+
     </div>
   );
 }
